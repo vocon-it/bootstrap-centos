22a3b748c8ed66348aeda341d52cc2a4e934c899
Oliver Veits <oveits@users.noreply.github.com>
2018-03-08 21:00:26 +0100

    vocon-latitude2.mooo.com

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$(cd $(dirname $0); pwd)/_$(basename $0) vocon-home.mooo.com vocon-latitude.mooo.com 195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77
+$(cd $(dirname $0); pwd)/_$(basename $0) vocon-home.mooo.com vocon-latitude.mooo.com vocon-latitude2.mooo.com	195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77
