5fa906b95e7bc05917b8ade92bc1e40b76665ba6
Oliver Veits <oliver.veits@web.de>
2018-03-19 22:01:22 +0100

    automatic git pull

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# update this file from git, if it has changed:
+( cd $(dirname $0) && git pull )
+
+# run _7xxx.sh:
 $(cd $(dirname $0); pwd)/_$(basename $0) vocon-home.mooo.com desktop-4j6l9uu.vocon-it.com vocon-latitude.mooo.com vocon-latitude2.mooo.com	195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77
