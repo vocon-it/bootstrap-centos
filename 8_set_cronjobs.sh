ef77749a2051f109e5c7e7cdc4dc8ee9970482a9
Oliver Veits <oliver.veits@web.de>
2018-03-19 22:00:30 +0100

    automatically set cronjob

---
+++ 8_set_cronjobs.sh
@@ -0,0 +1,18 @@
+
+#write out current crontab
+crontab -l > mycron
+
+#echo new cron into cron file
+NEWJOB="* * * * * /root/bootstrap-centos/7_create_iptables_entries.sh > /tmp/update-firewall.log 2>&1" 
+
+if ! crontab -l | grep "$NEWJOB" ; then
+   echo "$NEWJOB" >> mycron
+
+   #install new cron file
+   crontab mycron
+else
+   echo "Job was installed already"
+fi
+
+# cleaning
+rm mycron
