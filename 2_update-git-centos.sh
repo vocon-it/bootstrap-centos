20916ca194d7ce146746bf7b75545d78e604e51a
Oliver Veits <oveits@users.noreply.github.com>
2018-10-03 19:10:32 +0200

    wandisco: Install GIT from RPM (quicker)

--- 2_update-git-centos.sh
+++ 2_update-git-centos.sh
@@ -1,3 +1,11 @@
+# new method found on https://stackoverflow.com/questions/21820715/how-to-install-latest-version-of-git-on-centos-7-x-6-x
+yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+yum install -y git
+git --version
+exit
+
+# old (create from source, takes longer):
+
 yum remove -y git
 yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker wget
 [ "$GIT_VERSION" == "" ] && export GIT_VERSION=2.14.2
