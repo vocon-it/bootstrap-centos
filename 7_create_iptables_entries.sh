7467c54f9fc1adb814e6ee2d17b8f294ca9f23f9
Oliver Veits <oveits@users.noreply.github.com>
2018-10-22 20:29:37 +0200

    fix git not found for git in /usr/bin instead of /usr/local/bin

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # update this file from git, if it has changed:
-( cd $(dirname $0) && /usr/local/bin/git pull )
+( cd $(dirname $0) && export PATH=$PATH:/usr/local/bin && git pull )
 
 # run _7xxx.sh:
 $(cd $(dirname $0); pwd)/_$(basename $0) ganesh.vocon-it.com 195.201.16.221 82.113.121.136 79.250.252.39 82.113.121.50 vocon-home.mooo.com desktop-4j6l9uu-1.vocon-it.com vocon-latitude.mooo.com vocon-latitude2.mooo.com	195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77
