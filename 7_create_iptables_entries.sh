3c76f1661946ebadac8b3ca831fc7bfaf0fcb8aa
Oliver Veits <oliver.veits@gmail.com>
2019-01-15 20:40:43 +0100

    allow command line argument for 7_create_iptables_entries.sh

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ "$#" == "0" ]; then
+  MY_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
+  ADDIP="$MY_IP ganesh.vocon-it.com vocon-home.mooo.com"
+else
+  ADDIP="$@"
+fi
+
 # update this file from git, if it has changed:
 ( cd $(dirname $0) && export PATH=$PATH:/usr/local/bin && git pull )
 
@@ -8,7 +15,7 @@
 read -t 10 -p "Is this a DC/OS installation? (no) > "
 [ "$answer" == "y" -o "$answer" == "yes" ] \
    && echo "Okay, the DC/OS IP addresses are added to the Firewall table..." \
-   && ADDIP=" 195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77"
+   && ADDIP="$ADDIP 195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77"
 
 # run _7xxx.sh:
-$(cd $(dirname $0); pwd)/_$(basename $0) ganesh.vocon-it.com vocon-home.mooo.com $ADDIP
+$(cd $(dirname $0); pwd)/_$(basename $0) $ADDIP
