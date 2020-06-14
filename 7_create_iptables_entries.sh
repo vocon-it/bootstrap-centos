bc83779ccd1224ce0e692f772fda3f78ca6ab905
Oliver Veits <oliver.veits@gmail.com>
2020-06-14 13:32:52 +0200

    7.*.config file for disabling the firewall update

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# find and apply config file, if present
+CONFIG_FILE=${0%.*}.config
+[ -r "$CONFIG_FILE" ] \
+  && source $CONFIG_FILE \
+  && [ "$DEBUG" == "true" ] \
+  && echo "$0: read config file $CONFIG_FILE with following content:" \
+  && cat $CONFIG_FILE
+
+# DEFAULTS
+FIREWALL_UPDATES=${FIREWALL_UPDATES:=true}
+
+# update this file from git, if it has changed:
+( cd $(dirname $0) && export PATH=$PATH:/usr/local/bin && git pull )
+
+if [ "$FIREWALL_UPDATES" == "false" ]; then
+  echo "Firewall updates are disabled"
+  exit 0
+fi
+
 if [ "$#" == "0" ]; then
   MY_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
   ADDIP="$MY_IP grodrigues.vocon-it.com ganesh.vocon-it.com vocon-home.mooo.com dev-master1.vocon-it.com dev-node1.vocon-it.com dev-node2.vocon-it.com static.254.247.47.78.clients.your-server.de"
@@ -9,9 +28,6 @@ fi
 
 # is evaluated by _7_create_iptables_entries.sh
 export ENABLE_PUBLIC_WEB_ACCESS=false
-
-# update this file from git, if it has changed:
-( cd $(dirname $0) && export PATH=$PATH:/usr/local/bin && git pull )
 
 # User input: Add DC/OS IP addresses, if needed 
 # (this section is ignored if run via cron):
