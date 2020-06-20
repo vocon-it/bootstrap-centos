a7d073a67b0660b9ef727b12021cb3828dcc9085
Oliver Veits <oliver.veits@gmail.com>
2020-06-20 17:46:29 +0200

    config files in .7_create_iptables_entries directory

--- 7_create_iptables_entries.sh
+++ 7_create_iptables_entries.sh
@@ -1,10 +1,318 @@
 #!/bin/sh
+#
+# Add dynamic and static IP addresses to the Firewall
+#
+# Concept:
+# - all ACCEPT rules are placed into a CUSTOM-ACCEPT chain
+# - CUSTOM-ACCEPT chain has a last entry to RETURN to the calling chain
+# - CUSTOM-ACCEPT is placed as line #1 of INPUT and FORWARD chains
+# - most of the DROP rulse are placed into CUSTOM-DROP chain
+# - CUSTOM-DROP chain has a last entry to RETURN to the calling chain
+# - CUSTOM-DROP is placed as line #2 of the FORWARD chain
+# - some REJECT and LOG rules are still placed at the end of INPUT chain
+# TODO: will we keep some REJECT and LOG rules at the end of the INPUT chain?
+
+# for logging:
+date
 
 # update this file from git, if it has changed:
 ( cd $(dirname $0) && export PATH=$PATH:/usr/local/bin && git pull )
 
+definitions(){
+  IPTABLES=${IPTABLES:=/usr/sbin/iptables}
+  sudo echo hello 2>/dev/null 1>/dev/null || alias sudo='$@'
+}
+
+create_iptables_chains(){
+  definitions
+
+  if [ $# -le 0 ]; then
+    echo "usage: $0 CHAIN_1 [CHAIN_2 ...]"
+    return 1
+  fi
+
+  for CHAIN in $@; do
+    # Create the chain, if it does not exist:
+    if ! sudo $IPTABLES -n -L $CHAIN 2>/dev/null 1>/dev/null; then
+      sudo $IPTABLES -N $CHAIN && echo "$CHAIN created" || return 1
+    fi
+  done
+}
+
+modify_iptable_chain_policy() {
+  definitions
+
+  if [ $# -le 1 ]; then
+    echo "usage: $0 POLICY CHAIN_1 [CHAIN_2 ...]"
+    return 1
+  fi
+
+  POLICY=$1
+  shift
+  CHAINS=$@
+
+  for CHAIN in $CHAINS; do
+    # set default policy:
+    if [ "$POLICY" == "DROP" -o "$POLICY" == "ACCEPT" ] \
+    && [ "$CHAIN" == "INPUT" -o "$CHAIN" == "FORWARD" -o "$CHAIN" == "OUTPUT" ]; then
+      # default CHAIN policy in case of DROP and ACCEPT and built-in chain, add policy, if not already present
+      $IPTABLES -S "$CHAIN" | head -1 | egrep -q -v "^-P $CHAIN $POLICY$" \
+        && sudo $IPTABLES -P $CHAIN $POLICY || return 1 && echo "default policy ${POLICY} added to $CHAIN"
+    elif ! sudo $IPTABLES -n -L $CHAIN | egrep -q "^${POLICY}[ ]+"; then
+      sudo $IPTABLES -A $CHAIN -j ${POLICY} && echo "default policy ${POLICY} added to $CHAIN as explicit rule"
+    fi
+  done
+}
+
+# Insert a rule at position $INSERT_AT_LINE_NUMBER, if it is not found at that place:
+insert_rule_at_line() {
+  usage() {
+    echo "usage: [IPTABLES=...;] [INSERT_AT_LINE_NUMBER=...;] CHAIN=...; JUMP=...; ${FUNCNAME[1]}"
+    echo "       ${FUNCNAME[1]} CHAIN INSERT_AT_LINE_NUMBER JUMP"
+    echo
+    echo "       Note: INSERT_AT_LINE_NUMBER=0 means 'at the last line'"
+  }
+
+  clean_from_lower_line_duplicates() {
+    # clean $CHAIN from ${JUMP} duplicates found on lower line numbers (up to 3 duplicates)
+    for i in $(seq 1 3); do
+      if [ $($IPTABLES -n -L $CHAIN | egrep -c "^${JUMP}[ ]+all[ -]+0.0.0.0/0[ ]+0.0.0.0/0[ ]+$") -gt 1 ]; then
+        [ "$DEBUG" == "true" ] && echo "$0: ${FUNCNAME[0]}: found duplicate entry; deleting: $IPTABLES -D $CHAIN -j ${JUMP}"
+        $IPTABLES -D $CHAIN -j ${JUMP}
+      else
+        break
+      fi
+    done
+  }
+
+  clean() {
+    unset CHAIN CHAINS INSERT_AT_LINE_NUMBER
+  }
+
+  clean_return() {
+    clean
+    return $1
+  }
+
+  IPTABLES=${IPTABLES:=/usr/sbin/iptables}
+
+  if [ "$#" == "0" ]; then
+    CHAIN=${CHAIN:=NOT_DEFINED}
+    INSERT_AT_LINE_NUMBER=${INSERT_AT_LINE_NUMBER:=1}
+    JUMP=${JUMP:=NOT_DEFINED}
+  elif [ "$#" == "3" ]; then
+    CHAIN=$1
+    INSERT_AT_LINE_NUMBER=$2
+    JUMP=$3
+  else
+    usage
+    clean && return 1
+  fi
+  [ "$CHAIN" == "NOT_DEFINED" ] && echo "CHAIN is not defined. Exiting..." && usage && clean && return 1
+  [ "$JUMP" == "NOT_DEFINED" ] && echo "JUMP is not defined. Exiting..." && usage && clean && return 1
+  EXIT_CODE=1
+
+  [ "$DEBUG" == "true" ] && echo -e "$0: ${FUNCNAME[0]}:\nIPTABLES=$IPTABLES\nCHAIN=$CHAIN\nINSERT_AT_LINE_NUMBER=$INSERT_AT_LINE_NUMBER\nJUMP=$JUMP"
+
+  if [ "$INSERT_AT_LINE_NUMBER" == "0" ]; then
+    # check, if the current chain has an explicit policy rule:
+    $IPTABLES -S $CHAIN | tail -n 1 | egrep -q "^-A $CHAIN -j " && INSERT_AT_LINE_NUMBER="$(expr $($IPTABLES -S $CHAIN | wc -l) - 1)"
+    if [ "$INSERT_AT_LINE_NUMBER" != "0" ]; then
+      # line number has changed;
+      [ "$DEBUG" == "true" ] && echo "INSERT_AT_LINE_NUMBER has changed: $INSERT_AT_LINE_NUMBER"
+      insert_rule_at_line && return 0 || return 1
+    fi
+    # exit function, if rule exists at the last linie already:
+    if $IPTABLES -n -L ${CHAIN} --line-numbers \
+       | tail -n 1 | egrep -q "^[0-9]+[ ]*${JUMP}"; then
+       [ "$DEBUG" == "true" ] && echo "iptables rule exists already"
+       clean_from_lower_line_duplicates
+       clean && return 0
+    else
+      # create the entry
+      [ "$DEBUG" == "true" ] && echo "$0: ${FUNCNAME[0]}: Appending entry: $IPTABLES -A ${CHAIN} -j ${JUMP}"
+      $IPTABLES -A ${CHAIN} -j ${JUMP} && EXIT_CODE=0
+      clean_from_lower_line_duplicates
+    fi
+
+  elif [ $INSERT_AT_LINE_NUMBER -gt 0 ]; then
+    # exit function, if rule exists on specified line number already:
+    if $IPTABLES -n -L ${CHAIN} --line-numbers \
+       | egrep -q "^${INSERT_AT_LINE_NUMBER}[ ]*${JUMP}"; then
+       [ "$DEBUG" == "true" ] && echo "iptables rule exists already: $IPTABLES -I ${CHAIN} ${INSERT_AT_LINE_NUMBER} -j ${JUMP}"
+       clean
+       return 0
+    fi
+    # create the entry
+    [ "$DEBUG" == "true" ] && echo "Inserting entry at line: $IPTABLES -I ${CHAIN} ${INSERT_AT_LINE_NUMBER} -j ${JUMP}"
+    $IPTABLES -I ${CHAIN} ${INSERT_AT_LINE_NUMBER} -j ${JUMP} && EXIT_CODE=0
+  fi
+
+  # evaluate the success:
+  if [ "$EXIT_CODE" == "1" ]; then
+    echo "$0: ${FUNCNAME[0]}: Failed to apply following command: ${FUNCNAME[0]} $@"
+    clean && return 1
+  fi
+
+  clean && return 0
+}
+
+is_rule_present() {
+  definitions
+  # usage: $0 <rule-in-S-notation>
+  #        returns true or false
+  __CHAIN=$(echo $RULE | awk '{print $2}')
+  $IPTABLES -S $__CHAIN | egrep -q "^$(echo $@ | sed 's_-_\\-_g')$"
+}
+
+#-----------------
+# manual test of is_rule_present()
+#RULE="-A  CUSTOM-ACCEPT -s 192.168.0.0/16 -j ACCEPT"
+#is_rule_present $RULE && echo rule is present
+#exit 0
+#-----------------
+
+update_iptables_chains() {
+  for __CHAIN in $@; do
+    update_iptables_chain $__CHAIN
+  done
+  unset __CHAIN
+}
+
+update_iptables_chain() {
+  #
+  # update of a chain based on a <chain>.config file
+  #   the file can contain domain names instead of IP addresses.
+  #   if this functin is called periodically, it is made sure that the DNS to IP address binding is kept up to date
+  #
+
+  usage() {
+    echo "usage: $0: ${FUNCNAME[1]} <chain>"
+    echo "       file with name <chain>.config must exist"
+  }
+
+  # Definition of internal variables:
+  IPTABLES=${IPTABLES:=/usr/sbin/iptables}
+  __CHAIN=$1
+  __CONFIG_DIR=${CONFIG_DIR:=$(dirname $0)}
+  __CONFIG_FILE=${__CHAIN}.config
+  __TMP=${TMP:=/tmp}
+
+  # Input validation
+  [ "$#" != "1" ] && usage && return 1
+  [ ! -r "${__CONFIG_DIR}/${__CONFIG_FILE}" ] && echo "File ${__CONFIG_DIR}/${__CONFIG_FILE} not found on $(pwd)" && return 1
+
+  [ "$DEBUG" == "true" ] && echo "$0: ${FUNCNAME[0]}: Chain ${__CHAIN} has follwing config"
+  [ "$DEBUG" == "true" ] && cat ${__CONFIG_DIR}/${__CONFIG_FILE}
+
+  # create/flush TEMP-CHAIN chain
+  $IPTABLES -N TEMP-CHAIN 2>/dev/null
+  $IPTABLES -F TEMP-CHAIN
+
+  # number or rules for plausibility:
+  NUMBER_OF_CONFIG_LINES=$(cat "${__CONFIG_DIR}/${__CONFIG_FILE}" | egrep -v "^#" | egrep -v "^[ ]*$" | grep -c '\-j')
+  [ "$NUMBER_OF_CONFIG_LINES" == "0" ] && echo "$0: ${FUNCNAME[0]}: No configuration found. Returning..." && return 1
+
+  # create TEMP-CHAIN rules from ${__CONFIG_DIR}/${__CONFIG_FILE} file:
+  cat ${__CONFIG_DIR}/${__CONFIG_FILE} \
+    | envsubst \
+    | egrep -v "^#" \
+    | egrep -v "^[ ]*$" \
+    | awk -F '-A [^ ]* ' '{print $2}' \
+    | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'  \
+    | xargs -L 1 $IPTABLES -A TEMP-CHAIN
+  # export TEMP-CHAIN chain to ${__CONFIG_DIR}/${__CONFIG_FILE}.resolved file
+  $IPTABLES -S TEMP-CHAIN | sed "s/TEMP-CHAIN/${__CHAIN}/g" > "${__TMP}/${__CONFIG_FILE}.resolved"
+
+  # Plausibility check:
+  NUMBER_OF_CONFIG_LINES_UPDATED=$(cat "${__TMP}/${__CONFIG_FILE}.resolved" | grep -c '\-j')
+  [ "$NUMBER_OF_CONFIG_LINES" != "$NUMBER_OF_CONFIG_LINES_UPDATED" ] \
+    && echo "$0: ${FUNCNAME[0]}: ERROR: Only $NUMBER_OF_CONFIG_LINES_UPDATED of all config lines ($NUMBER_OF_CONFIG_LINES) were successful. Therefore the chain $__CHAIN will not be updated at all. Returning..." \
+    && echo "config file:" \
+    && cat "${__CONFIG_DIR}/${__CONFIG_FILE}" \
+    && echo "result:" \
+    && cat "${__TMP}/${__CONFIG_FILE}.resolved" \
+    && return 1
+
+  # save current ${__CHAIN} rules to a file, so it can be compared to the updated version
+  $IPTABLES -S ${__CHAIN} > "${__TMP}/${__CHAIN}.save";
+
+  if ! diff "${__TMP}/${__CHAIN}.save" "${__TMP}/${__CONFIG_FILE}.resolved" >/dev/null; then
+    # files are different; therefore the chain must be replaced by the resolved version of the configured chain
+    $IPTABLES -F ${__CHAIN} \
+      && cat "${__TMP}/${__CONFIG_FILE}.resolved" | xargs -L 1 $IPTABLES
+      echo "$0: ${FUNCNAME[0]}: iptables chain ${__CHAIN} updated"
+      [ "$DEBUG" == "true" ] && $IPTABLES -S ${__CHAIN}
+  else
+    [ "$DEBUG" == "true" ] && echo "$0: ${FUNCNAME[0]}: iptables chain $CHAIN and the FQDNs therein have not changed"
+  fi
+  [ "$DEBUG" == "true" ] && echo "$0: ${FUNCNAME[0]}: iptables chain $CHAIN content after update:"
+  [ "$DEBUG" == "true" ] && $IPTABLES -S $CHAIN
+
+
+  # cleaning
+  $IPTABLES -F TEMP-CHAIN; $IPTABLES -X TEMP-CHAIN
+}
+
+numberOfDuplicateLines(){
+ sudo iptables-save > /tmp/iptables-save \
+   && cat /tmp/iptables-save \
+   | awk '/^COMMIT|^:|^#/ {print $0} !/^COMMIT|^:|^#/ && !x[$0]++' > /tmp/iptables-save.awked \
+   && diff /tmp/iptables-save.awked /tmp/iptables-save | wc -l
+}
+
+remove_duplicate_entries_from_iptables() {
+
+  if [ "$(numberOfDuplicateLines)" == "0" ]; then
+    echo "No duplicate lines found in iptables"
+  else
+    echo "Removing duplicate lines from iptables"
+    # debugging:
+    [ "$DEBUG" == "true" ] && echo "BEFORE:" && sudo iptables -n -L INPUT
+    [ "$DEBUG" == "true" ] && echo "duplicates: " && diff /tmp/iptables-save.awked /tmp/iptables-save
+    sudo iptables-save \
+      | awk '/^COMMIT|^:|^#/ {print $0} !/^COMMIT|^:|^#/ && !x[$0]++' \
+      | sudo iptables-restore \
+      && echo "Removed duplicate lines from iptables"
+    [ "$?" != "0" ] && echo "failed to remove duplicate lines in iptables" && exit 1 || true
+    # debugging:
+    [ "$DEBUG" == "true" ] && echo "AFTER:" && sudo iptables -n -L INPUT
+  fi
+
+}
+
+#-----------------
+# manual test of update_iptables_chain()
+#create_iptables_chains CUSTOM-FORWARD-HEAD
+#update_iptables_chain CUSTOM-FORWARD-HEAD
+#exit 0
+#-----------------
+
+
+####################
+#      MAIN        #
+####################
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script ($0) must be run as root. Exiting..."
+  exit 1
+fi
+
+usage(){
+  echo "usage: $0"
+}
+
+if [ "$#" != "0" ]; then
+  usage
+  exit 1
+fi
+
+#############
 # find and apply config file, if present
-CONFIG_FILE=${0%.*}.config
+#############
+CONFIG_DIR=${CONFIG_DIR:=$(echo ${0%.*} | sed 's_\([^/]*$\)_.\1_')}
+CONFIG_FILE=$CONFIG_DIR/config
+#$(echo ${0%.*} | sed 's_/\([^/]*$\)_/.\1_')/config
 [ -r "$CONFIG_FILE" ] \
   && source $CONFIG_FILE \
   && [ "$DEBUG" == "true" ] \
@@ -13,49 +321,66 @@ CONFIG_FILE=${0%.*}.config
 
 # DEFAULTS
 ENABLED=${ENABLED:=true}
+definitions
 
+#############
+# Exit, if function is disabled
+#############
 if [ "$ENABLED" == "false" ]; then
   echo "Firewall updates are disabled"
   exit 0
 fi
 
-# Adapt List of IP addresses / FQDNs to be allowed by firewall rules:
-if [ "$#" == "0" ]; then 
-  # programm is called with no arguments
-  MY_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
-  # prepend your own SSH source IP, if present:
-  export ADDIP="$MY_IP $ADDIP"
-else 
-  # program is called with arguments; use those arguments only
-  ADDIP="$@"
-fi
+#############
+# Add current SSH IP address to list of allowed IP addresses (does not work in the moment, since we do not allow for adding config lines programmatically any more)
+#############
+# TODO: create a function to re-write *.config files
+#############
+## Adapt List of IP addresses / FQDNs to be allowed by firewall rules:
+#if [ "$#" == "0" ]; then
+#  # programm is called with no arguments
+#  MY_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
+#  # prepend your own SSH source IP, if present:
+#  export ADDIP="$MY_IP $ADDIP"
+#else
+#  # program is called with arguments; use those arguments only
+#  ADDIP="$@"
+#fi
 
-# User input: Add DC/OS IP addresses, if needed 
-# (this section is ignored if run via cron):
 
-if [ "$KUBERNETES" == "" ]; then
-  answer=no
-  read -t 10 -p "Is this a Kubernetes installation? (no) > " answer
-  [ "$answer" == "y" -o "$answer" == "yes" ] \
-    && echo "Okay, the Kubernetes IP addresses are added to the Firewall table..." \
-    && ADDIP="$ADDIP 10.96.0.1" \
-    && export KUBERNETES=true # Note: $KUBERNETES is needed by_7_create_iptables_entries.sh
-fi
+#############
+# make sure sudo is defined (create an alias, if needed)
+#############
+sudo echo hello 2>/dev/null 1>/dev/null || alias sudo='$@'
 
-if [ "$DCOS" == "" ]; then
-  answer=no
-  read -t 10 -p "Is this a DC/OS installation? (no) > " answer
-  [ "$answer" == "y" -o "$answer" == "yes" ] \
-    && echo "Okay, the DC/OS IP addresses are added to the Firewall table..." \
-    && ADDIP="$ADDIP 195.201.27.175 195.201.17.1 94.130.187.229 195.201.30.230 94.130.186.77" \
-    && export DCOS=true # Note: $DCOS is needed by_7_create_iptables_entries.sh
-fi
+#############
+# Install bind-utils if not present:
+#############
+sudo yum list installed | grep -q bind-utils || sudo yum install -y bind-utils
 
-# run _7xxx.sh:
-$(cd $(dirname $0); pwd)/_$(basename $0) $ADDIP
+#############
+# Add INPUT rules
+#############
+create_iptables_chains CUSTOM-ACCEPT CUSTOM-DROP CUSTOM-TAIL
+update_iptables_chains CUSTOM-ACCEPT CUSTOM-DROP CUSTOM-TAIL
+insert_rule_at_line INPUT 1 CUSTOM-ACCEPT
+insert_rule_at_line INPUT 2 CUSTOM-DROP
+insert_rule_at_line INPUT 0 CUSTOM-TAIL
 
-# remove duplicate entries:
-$(cd $(dirname $0); pwd)/_7_2_remove_duplicate_iptable_entries.sh
+#############
+# Add FORWARD rules
+#############
+create_iptables_chains CUSTOM-FORWARD-HEAD
+update_iptables_chains CUSTOM-FORWARD-HEAD
+insert_rule_at_line FORWARD 1 CUSTOM-FORWARD-HEAD
 
-IPTABLES=${IPTABLES:=/usr/sbin/iptables}
-$IPTABLES -P INPUT ACCEPT
+#############
+# Default ACCEPT policy on INPUT chain
+#############
+modify_iptable_chain_policy ACCEPT INPUT
+
+#############
+# remove duplicate entries form iptables
+#############
+remove_duplicate_entries_from_iptables
+
