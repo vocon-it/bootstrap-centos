3c84ed9133feca59540ae518318fb0601215ecc7
Rodrigues-Gabriel <52155147+Rodrigues-Gabriel@users.noreply.github.com>
2020-07-13 17:20:54 -0300

    changing from arguments to environment variables on script 1 (#5)

--- 1_setup_user.sh
+++ 1_setup_user.sh
@@ -1,41 +1,35 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]; then
-    USER=centos
-else 
-    if [ $# -eq 1 ]; then
-        USER=$1
-    else
-        echo "usage: $0 [USER]"
+CREATEUSER=${CREATEUSER:=centos}
+if [ $# -ne 0 ]; then
+        echo "usage: [CREATEUSER=myuser] $0"
         echo "       default user is centos"
         exit 1
-    fi
 fi
 
-echo "Creating user $USER"
+echo "Creating user $CREATEUSER"
 
 # create user, if it does not exist:
-if cut -d: -f1 /etc/passwd | grep -q -E "^${USER}$"; then
-   echo "User $USER exists already" 
+if cut -d: -f1 /etc/passwd | grep -q -E "^${CREATEUSER}$"; then
+   echo "User $CREATEUSER exists already"
 else
-   adduser $USER
+   adduser $CREATEUSER
 fi
 
 # initialize the SSH keys, if the authorized_keys is new:
-[ -d /home/$USER/.ssh ] ||  mkdir -p /home/$USER/.ssh
-if [ ! -f /home/$USER/.ssh/authorized_keys ]; then
-   touch /home/$USER/.ssh/authorized_keys
-   cat /root/.ssh/authorized_keys >> /home/$USER/.ssh/authorized_keys
+[ -d /home/$CREATEUSER/.ssh ] ||  mkdir -p /home/$CREATEUSER/.ssh
+if [ ! -f /home/$CREATEUSER/.ssh/authorized_keys ]; then
+   touch /home/$CREATEUSER/.ssh/authorized_keys
+   cat /root/.ssh/authorized_keys >> /home/$CREATEUSER/.ssh/authorized_keys
 fi
 
 # fix ownership and permissions:
-chown -R centos:centos /home/$USER/.ssh
-chmod 600 /home/$USER/.ssh/authorized_keys
+chown -R centos:centos /home/$CREATEUSER/.ssh
+chmod 600 /home/$CREATEUSER/.ssh/authorized_keys
 
 # allow user to perform sudo without password, if not already done:
-PRIVILEGES=$(cat /etc/sudoers | grep -E "^${USER} " | awk '{ $1=""; print}')
+PRIVILEGES=$(cat /etc/sudoers | grep -E "^${CREATEUSER} " | awk '{ $1=""; print}')
 [ "$PRIVILEGES" != "" ] \
-   && echo "User $USER already has following sudo privileges:$PRIVILEGES" \
-   || echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
+   && echo "User $CREATEUSER already has following sudo privileges:$PRIVILEGES" \
+   || echo "$CREATEUSER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
