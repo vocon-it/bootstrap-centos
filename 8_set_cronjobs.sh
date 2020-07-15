1a7fd45989bae5b777a482d4498039b8f56fc8cb
Oliver Veits (vocon IT) <oliver.veits@vocon-it.com>
2020-07-15 22:20:42 +0200

    bugfix for 8_... cron job creation

--- 8_set_cronjobs.sh
+++ 8_set_cronjobs.sh
@@ -2,8 +2,10 @@
 #write out current crontab
 crontab -l > mycron
 
+PWD=$(cd $(dirname $0); pwd) 
+
 #echo new cron into cron file
-NEWJOB="* * * * * /root/bootstrap-centos/7_create_iptables_entries.sh > /tmp/update-firewall.log 2>&1" 
+NEWJOB="* * * * * $PWD/7_create_iptables_entries.sh > /tmp/update-firewall.log 2>&1" 
 
 if ! crontab -l | grep "$NEWJOB" ; then
    echo "$NEWJOB" >> mycron
