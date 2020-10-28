4796392aeb55974973f4e6c1dd5b63b42416db70
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-10-28 17:32:24 +0000

    5 ways of installing git on CentOS 6 and 7

--- 2_update-git-centos.sh
+++ 2_update-git-centos.sh
@@ -1,147 +1,128 @@
-# Install GIT via Rackspace IUS directly from rpm:
+# 5 Ways to install GIT on CentOS 6/7
+# They are tried one after the other
+# - binary from IUS Repo (repo.ius.io)
+# - binary from packages.endpoint.com repo
+# - binary directly from RPM of IUS repo
+# - binary from wandisco repo (opensource.wandisco.com)
+# - from source (www.kernel.org/pub/software/scm/git)
 
-sudo echo nothing 2>/dev/null 1>/dev/null || alias sudo=''
-sudo yum remove -y git*
-sudo yum list installed | grep ius-release-el7 \
-    || sudo yum install -y  https://repo.ius.io/ius-release-el7.rpm
-sudo yum install -y git-core
-git --version 2>/dev/null | egrep "[2-9]\.[0-9]+\.[0-9]+" 2>/dev/null \
-  && exit 0 \
-  || exit 1
+CENTOS_MAIN_VERSION=$(cat /etc/centos-release | awk '{print $4}' | awk -F '.' '{print $1}')
 
-############################
+URL_IUS=https://repo.ius.io/ius-release-el${CENTOS_MAIN_VERSION}.rpm
+GIT_PACKAGE_IUS=git-core
 
-# sudo errors below this line...
+URL_PACKAGES=https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
+GIT_PACKAGE_PACKAGES=git
+
+URL_IUS_GIT_RPM=https://repo.ius.io/7/x86_64/packages/g/git224-core-2.24.3-1.el7.ius.x86_64.rpm
+
+URL_WANDISCO=http://opensource.wandisco.com/centos/${CENTOS_MAIN_VERSION}/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+GIT_PACKAGE_WANDISCO=git
+
+GIT_VERSION_FROM_SOURCE=${GIT_VERSION_FROM_SOURCE:=2.29.1}
+BASE_URL_GIT_SOURCE_REPO="https://www.kernel.org/pub/software/scm/git"
 
 define_sudo() {
-  sudo echo nothing 2>/dev/null 1>/dev/null || alias sudo=''
+  unalias sudo 2>/dev/null
+  if ! sudo echo nothing 2>/dev/null 1>/dev/null; then 
+    sudo() { $@; }; 
+  fi
 }
-export -f define_sudo
 
 remove_git() {
   define_sudo
   sudo yum remove -y git* || true
 }
-export -f remove_git
 
-install_ius_repo() {
-  define_sudo
-  sudo yum list installed | grep ius-release-el7 \
-    || sudo yum install -y  https://repo.ius.io/ius-release-el7.rpm
+package_pattern_from_url() {
+  _url=${1}
+  echo ${_url} | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}'
 }
-export -f install_ius_repo
 
-install_endpoint_repo() {
+install_repo_from_url() {
+  _url=${1}
   define_sudo
-  sudo yum list installed | grep endpoint-repo \
-    || sudo yum install -y  https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
+  sudo yum list installed | grep $(package_pattern_from_url ${_url}) \
+    || sudo yum install -y ${_url}
 }
-export -f install_endpoint_repo
 
-install_wandisco_repo() {
-  define_sudo
-  sudo yum list installed | grep endpoint-repo \
-    || sudo yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+print_git_version() {
+  git --version 2>/dev/null
 }
-export -f install_wandisco_repo
 
 verify_git_version() {
-  git --version 2>/dev/null | egrep "[2-9]\.[0-9]+\.[0-9]+" 2>/dev/null 1>/dev/null
+  print_git_version | egrep "[2-9]\.[0-9]+\.[0-9]+" 2>/dev/null 1>/dev/null
 }
-export -f verify_git_version
 
-git_install() {
+yum_install() {
   define_sudo
   sudo yum install -y $@
 }
-export -f git_install
 
+install_git_from_url() {
+  _package=${1}
+  _url=${2}
+  if install_repo_from_url ${_url}; then
+    define_sudo
+    remove_git
+    yum_install ${_package}
+  fi
+}
 
-if verify_git_version; then
-  echo "git v2+ already installed"
-  true
-fi
-
-if install_ius_repo; then
+install_git_from_source() {
+  _version=${1}
   define_sudo
-#  sudo echo nothing 2>/dev/null 1>/dev/null || alias sudo='$@'
   remove_git
-  git_install git-core \
+  yum install -y make curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker wget
+  mkdir /root/git \
+  && cd /root/git \
+  && wget "${BASE_URL_GIT_SOURCE_REPO}/git-${_version}.tar.gz" \
+  && tar xvzf "git-${_version}.tar.gz" \
+  && cd git-${_version} \
+  && make prefix=/usr all \
+  && make prefix=/usr install
+}
+
+############
+### MAIN ###
+############
+
+# exit, if git has correct version
+verify_git_version \
+  && echo "git v2+ already installed" \
+  && print_git_version \
+  && exit 0
+
+# install git from IUS repo and exit on success
+install_git_from_url ${GIT_PACKAGE_IUS} ${URL_IUS} \
+  && print_git_version \
   && verify_git_version \
   && exit 0
-fi
 
-export -f define_sudo
-export -f remove_git
-export -f verify_git_version
-
-if install_endpoint_repo; then
-  export -f remove_git
-  export -f verify_git_version
-  remove_git \
-  && sudo yum install -y git \
+# install git from packages repo and exit on success
+install_git_from_url ${GIT_PACKAGE_PACKAGES} ${URL_PACKAGES} \
+  && print_git_version \
   && verify_git_version \
   && exit 0
-fi
 
+# install directly from IUS PRM and exit on success
+yum_install ${URL_IUS_GIT_RPM} \
+  && print_git_version \
+  && verify_git_version \
+  && exit 0
+
+# install git from wandisco repo and exit on success
 CENTOS_MAIN_VERSION=$(cat /etc/centos-release | awk '{print $4}' | awk -F '.' '{print $1}')
-if [ "${CENTOS_MAIN_VERSION}" == "7" ] && install_wandisco_repo; then
-  remove_git \
-  && sudo yum install -y git \
-  && verify_git_version \
-  && exit 0
-fi
-
-exit 1
-
-
-# Install git
-if sudo yum list installed | grep ius-release-el7; then
-  sudo yum remove -y git*
-  sudo yum install -y git-core \
-  && git --version | egrep "[2-9]\.[0-9]+\.[0-9]+" \
-  && exit 0
-fi
-
-sudo yum remove git*
-sudo yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm \
-  && sudo yum install git \
-  && git --version \
-  && exit 0
-
-sudo yum install -y https://repo.ius.io/7/x86_64/packages/g/git224-core-2.24.3-1.el7.ius.x86_64.rpm \
-  && git --version \
-  && exit 0
-
-
-
-# Was not successful? Then try via the Wandisco Repo:
-
-# new method found on https://stackoverflow.com/questions/21820715/how-to-install-latest-version-of-git-on-centos-7-x-6-x
-# I have seen major problems with CentoS 8, so we will apply it only for CentOS 7:
-CENTOS_MAIN_VERSION=$(cat /etc/centos-release | awk '{print $4}' | awk -F '.' '{print $1}')
-
 [ "${CENTOS_MAIN_VERSION}" == "7" ] \
-  && echo "Not successfully tested for CentOS version ${CENTOS_MAIN_VERSION}. Skipping ..." \
-  && yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm \
-  && yum install -y git \
-  && git --version \
+  && install_git_from_url ${GIT_PACKAGE_WANDISCO} ${URL_WANDISCO} \
+  && print_git_version \
+  && verify_git_version \
+  && exit 0
+
+# install from source and exit on success
+install_git_from_source ${GIT_VERSION_FROM_SOURCE} \
+  && print_git_version \
+  && verify_git_version \
   && exit 0
 
 exit 1
-
-
-# old (create from source, takes longer):
-
-yum remove -y git
-yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker wget
-[ "$GIT_VERSION" == "" ] && export GIT_VERSION=2.14.2
-mkdir /root/git
-cd /root/git
-wget "https://www.kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.gz"
-tar xvzf "git-${GIT_VERSION}.tar.gz"
-cd git-${GIT_VERSION}
-make prefix=/usr all
-make prefix=/usr install
-#yum remove -y git
-git --version # should be GIT_VERSION
