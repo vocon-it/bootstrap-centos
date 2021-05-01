e280ee9016210e1a3beb87d8e7f93701233db770
Oliver Veits <oliver.veits@vocon-it.com>
2021-05-01 19:40:49 +0200

    CAAS-340 cron log all to /var/log/cron for better troubleshooting

--- 8_set_cronjobs.sh
+++ 8_set_cronjobs.sh
@@ -8,7 +8,8 @@ crontab -l > mycron || true
 PWD=$(cd $(dirname $0); pwd) 
 
 #echo new cron into cron file
-NEWJOB="* * * * * $PWD/7_create_iptables_entries.sh > /tmp/update-firewall.log 2>&1"
+#NEWJOB="* * * * * $PWD/7_create_iptables_entries.sh > /tmp/update-firewall.log 2>&1"
+NEWJOB="* * * * * $PWD/7_create_iptables_entries.sh 2>&1"
 
 if ! crontab -l | grep "$NEWJOB" ; then
   echo "$NEWJOB" >> mycron
