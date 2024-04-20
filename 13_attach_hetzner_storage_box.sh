2fecf803d38d736ad6712e2b87bd9f232d01c2d2
Oliver Veits <oveits@users.noreply.github.com>
2024-04-20 19:40:02 +0200

    bootstrap-centos: feature/CAAS-1745-attach-hetzner-storage-box (#18)
    * CAAS-1745 13_attach_hetzner_storage_box.sh
    
    * CAAS-1745 fixed 13_attach_hetzner_storage_box.sh and improved error handling
    
    * CAAS-1745 install cifs-utils
    
    * CAAS-1745 uid=1000 for hetzner storage box
    
    * CAAS-1745 added missing systemctl deamon-reload
    
    * CAAS-1745 added missing systemctl daemon-reload
    
    ---------
    
    Co-authored-by: Oliver Veits @ vocon <oliver.veits@vocon-it.com>
    Co-authored-by: oliver.veits@dev-node2.vocon-it.com <oliver.veits@dev-node2.vocon-it.com>

---
+++ 13_attach_hetzner_storage_box.sh
@@ -0,0 +1,31 @@
+
+sudo yum install -y cifs-utils
+
+HETZNER_STORAGE_USER=u380503
+sudo mkdir -p /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de
+sudo mkdir -p /etc/Hetzner/
+sudo touch /etc/Hetzner/${HETZNER_STORAGE_USER}.your-storagebox.de.credentials
+sudo chmod 600 /etc/Hetzner/${HETZNER_STORAGE_USER}.your-storagebox.de.credentials
+
+sudo cat /etc/Hetzner/${HETZNER_STORAGE_USER}.your-storagebox.de.credentials | grep -q username=${HETZNER_STORAGE_USER} \
+  || CREDENTIALS_FOUND=false
+
+[ "${CREDENTIALS_FOUND}" == "false" ] \
+  && echo "ERROR: cannot read file /etc/Hetzner/${HETZNER_STORAGE_USER}.your-storagebox.de.credentials or the file does not contain the username=${HETZNER_STORAGE_USER}. Exiting..." \
+  && exit 1
+
+if ! grep -q ${HETZNER_STORAGE_USER} /etc/fstab; then
+  echo "//${HETZNER_STORAGE_USER}.your-storagebox.de/backup /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de cifs vers=1.0,iocharset=utf8,rw,credentials=/etc/Hetzner/${HETZNER_STORAGE_USER}.your-storagebox.de.credentials,uid=1000,gid=1000,file_mode=0660,dir_mode=0770 0 0" \
+    | sudo tee -a /etc/fstab
+  sudo systemctl daemon-reload
+else
+  echo "INFO: Hetzner Storage Box ${HETZNER_STORAGE_USER} is already present in /etc/fstab. Nothing to do."
+fi
+
+if mount | grep -q ${HETZNER_STORAGE_USER}; then
+  echo "INFO: /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de is already mounted. Nothing to do."
+else
+  sudo mount /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de \
+    && echo "INFO: mounted /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de" \
+    || echo "ERROR: failed to mount  /mnt/${HETZNER_STORAGE_USER}.your-storagebox.de"
+fi
